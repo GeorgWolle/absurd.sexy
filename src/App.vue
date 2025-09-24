993736805e43c10d8224f90e9da3af0f73a0d3a6
Georg Wollmann <georg.wollmann@publicproject.at>
2025-09-24 16:19:01 +0200

    feat: magnifier

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,84 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted, onUnmounted } from "vue";
+
+const x = ref(0);
+const y = ref(0);
+
+const visible = ref(false);
+
+function updateCursor(e) {
+  x.value = e.clientX;
+  y.value = e.clientY;
+
+  if (
+    x.value - 630 > 20
+    && x.value - 630 < 205
+    && y.value - 260 > 50
+    && y.value - 260 < 250
+  ) {
+    visible.value = true;
+  } else {
+    visible.value = false;
+  }
+}
+
+onMounted(() => {
+  window.addEventListener("mousemove", updateCursor);
+  window.addEventListener("touchmove", updateCursor);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("mousemove", updateCursor);
+  window.removeEventListener("touchmove", updateCursor);
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div
+    class="
+      w-full h-[100vh] relative
+      flex items-center justify-center
+    "
+  >
+    <div
+      class="
+        text-lg scale-[0.2]
+      "
+    >
+      absurd
+    </div>
+    <div
+      class="
+        fixed w-[321px] h-[450px]
+      "
+      :style="{ left: x + 'px', top: y + 'px' }"
+      style="transform: translate(-50%, -50%);"
+    >
+      <img
+        class="absolute z-0"
+        src="/layer0.png"
+        alt="cursor"
+      />
+      <div
+        class="
+          absolute text-lg z-1
+        "
+        :class="{ hidden: !visible }"
+        :style="{ left: x - 630 + 'px', top: y - 260 + 'px' }"
+      >
+        absurd
+      </div>
+      <img
+        class="absolute z-2"
+        src="/layer1.png"
+        alt="cursor"
+      />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue absurd" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  #custom-cursor {
+    pointer-events: none; /* blockiert keine Klicks */
+  }
 </style>
