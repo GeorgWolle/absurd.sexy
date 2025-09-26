bfbe51b8463ccd1bd162b14f878d2191c0aedd31
GeorgWolle <wollmann.georg@gmail.com>
2025-09-26 23:14:17 +0200

    feat: magnifier responsive

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,95 @@
 <script setup>
+import { Input } from "postcss";
 import { ref, onMounted, onUnmounted } from "vue";
 
-const x = ref(0);
-const y = ref(0);
+const centerX = ref(window.innerWidth / 2);
+const centerY = ref(window.innerHeight / 2);
+const x = ref(window.innerWidth / 5);
+const y = ref(window.innerHeight / 50);
+
+const leave = ref(false);
+
+const visibleFromLeft = 80;
+const visibleFromRight = -145;
+const visibleFromTop = 288;
+const visibleFromBottom = 70;
+
+const magnifierCenterOffsetX = 145;
+const magnifierCenterOffsetY = -65;
 
 const visible = ref(false);
 
-function updateCursor(e) {
-  x.value = e.clientX;
-  y.value = e.clientY;
+const getCoords = e => {
+  if (e.type.startsWith("touch")) {
+    const touch = e.touches[0]; // erster Finger
+    return { x: touch.clientX - 80, y: touch.clientY - 320 };
+  } else {
+    return { x: e.clientX, y: e.clientY };
+  }
+};
+
+const updateCursor = e => {
+  const { x: cx, y: cy } = getCoords(e);
+  x.value = cx;
+  y.value = cy;
+
+  centerX.value = window.innerWidth / 2;
+  centerY.value = window.innerHeight / 2;
 
   if (
-    x.value - 630 > 20
-    && x.value - 630 < 205
-    && y.value - 260 > 50
-    && y.value - 260 < 250
+    centerX.value - x.value <= visibleFromLeft &&
+    centerX.value - x.value >= visibleFromRight &&
+    centerY.value - y.value <= visibleFromTop &&
+    centerY.value - y.value >= visibleFromBottom
   ) {
     visible.value = true;
   } else {
     visible.value = false;
   }
+};
+
+const handleGlobalClick = e => {
+  if (visible.value) {
+    leave.value = true;
+  }
+};
+
+function handleTwoFingerTap(e) {
+  if (e.touches.length === 2) {
+    if (visible.value) {
+      leave.value = true;
+    }
+  }
 }
 
 onMounted(() => {
   window.addEventListener("mousemove", updateCursor);
+  window.addEventListener("click", handleGlobalClick);
   window.addEventListener("touchmove", updateCursor);
+  window.addEventListener("touchstart", handleTwoFingerTap);
 });
 
 onUnmounted(() => {
   window.removeEventListener("mousemove", updateCursor);
+  window.removeEventListener("click", handleGlobalClick);
   window.removeEventListener("touchmove", updateCursor);
+  window.removeEventListener("touchstart", handleTwoFingerTap);
 });
 </script>
 
 <template>
   <div
+    v-if="leave"
+    class="
+      w-full h-[100vh] relative
+      flex items-center justify-center
+      text-4xl font-bold
+    "
+  >
+    Bravo!
+  </div>
+  <div
+    v-else
     class="
       w-full h-[100vh] relative
       flex items-center justify-center
@@ -42,17 +97,28 @@ onUnmounted(() => {
   >
     <div
       class="
+        flex flex-col items-center justify-center
         text-lg scale-[0.2]
       "
     >
       absurd
+
+      <button
+        class="
+          px-6 py-3 bg-indigo-600 text-white font-semibold rounded-lg shadow-md
+          hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-400
+          focus:ring-offset-2 active:scale-95 transition
+        "
+      >
+        Enter
+      </button>
     </div>
     <div
       class="
-        fixed w-[321px] h-[450px]
+        fixed w-[325px]
       "
       :style="{ left: x + 'px', top: y + 'px' }"
-      style="transform: translate(-50%, -50%);"
+      style="transform: translate(-50%, -50%); touch-action: none;"
     >
       <img
         class="absolute z-0"
@@ -61,12 +127,22 @@ onUnmounted(() => {
       />
       <div
         class="
-          absolute text-lg z-1
+          absolute flex flex-col items-center justify-center text-lg z-1
         "
         :class="{ hidden: !visible }"
-        :style="{ left: x - 630 + 'px', top: y - 260 + 'px' }"
+        :style="{ left: magnifierCenterOffsetX + (centerX - x) * .80 + 'px', top: magnifierCenterOffsetY + centerY - y * .80 + 'px' }"
       >
         absurd
+
+        <button
+          class="
+            px-6 py-3 bg-indigo-600 text-white font-semibold rounded-lg shadow-md
+            hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-400
+            focus:ring-offset-2 active:scale-95 transition
+          "
+        >
+          Enter
+        </button>
       </div>
       <img
         class="absolute z-2"
@@ -79,6 +155,6 @@ onUnmounted(() => {
 
 <style scoped>
   #custom-cursor {
-    pointer-events: none; /* blockiert keine Klicks */
+    pointer-events: none;
   }
 </style>
